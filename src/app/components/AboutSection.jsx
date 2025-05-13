2c4484186d64dcc37af86098a0db623082cefc79
swapan1264 <enquiries@treasuredcareforyou.com>
2025-05-13 08:15:59 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -12,7 +12,7 @@ export default function AboutSection() {
     <>
         {pathname === "/about" && <HeroSection />}
     <section>
-    <h2 className="text-4xl md:text-5xl font-extrabold my-16 text-center text-pink">
+    <h2 className="text-4xl md:text-5xl font-extrabold my-16 text-center text-pink font-heading">
           About Us
         </h2>
       <div className="mx-4 lg:mx-8 py-8 rounded-xl shadow-md bg-pink">
@@ -28,16 +28,16 @@ export default function AboutSection() {
 
           {/* Text side */}
           <div className="md:w-1/2 space-y-6">
-            <h3 className="text-2xl font-extrabold text-white text-center">
+            <h3 className="text-2xl font-extrabold text-white text-center font-heading">
               Why Treasured Care For You?
             </h3>
-            <p className="text-white leading-relaxed px-4">
+            <p className="text-white leading-relaxed px-4 font-heading">
               At Treasured Care For You, we believe in the importance of fostering a supportive and nurturing environment. Our team of experienced professionals are dedicated to delivering care that is not only effective but also compassionate and respectful. Additionally, we are experts in the National Disability Insurance Scheme (NDIS), with all our support workers having undergone NDIS screening and induction. Our team is also experienced in using hoists, Sara Stedy, and PEG feeding—ensuring comprehensive and skilled care for those who need it.
             </p>
-            <p className="text-white leading-relaxed px-4">
+            <p className="text-white leading-relaxed px-4 font-heading">
               We are also favoured in providing support to children with disabilities, and their families; allowing them some respite. Our services are designed to offer respite and peace of mind to families, knowing their loved ones are in our capable and caring hands.
             </p>
-            <p className="text-white leading-relaxed px-4">
+            <p className="text-white leading-relaxed px-4 font-heading">
               We offer low and affordable hourly rates, making it easier for you to receive the care you need most without the financial strain. Our sleepover services are also available, allowing you to access more hours of care when needed.
             </p>
           </div>
