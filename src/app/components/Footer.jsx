3ef009e5b0a7aede1b3fd8a7bf4854f9b28dc953
swapan1264 <enquiries@treasuredcareforyou.com>
2025-05-13 08:14:16 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
               alt="Tcare for you"
               className="h-32 w-auto mb-4 mx-auto"
             />
-            <span className="text-3xl font-extrabold text-white text-center">
+            <span className="text-3xl font-extrabold text-white text-center font-heading">
               Treasured Care For You
             </span>
           </div>
@@ -35,22 +35,22 @@ export default function Footer() {
 
         {/* Column 2 */}
         <div>
-          <h4 className="font-extrabold mb-3 text-2xl text-white">
+          <h4 className="font-extrabold mb-3 text-2xl text-white font-heading">
             Our services
           </h4>
           <ul className="space-y-2">
             <li>
-              <Link href="/vision" className="hover:underline">
+              <Link href="/vision" className="hover:underline font-heading">
                 Vision Mission
               </Link>
             </li>
             <li>
-              <Link href="/values" className="hover:underline">
+              <Link href="/values" className="hover:underlin font-headinge">
                 Company Values
               </Link>
             </li>
             <li>
-              <Link href="/about" className="hover:underline">
+              <Link href="/about" className="hover:underlin font-headinge">
                 About Us
               </Link>
             </li>
@@ -59,17 +59,17 @@ export default function Footer() {
 
         {/* Column 3 */}
         <div>
-          <h4 className="font-extrabold mb-3 text-2xl text-white">
+          <h4 className="font-extrabold mb-3 text-2xl text-white font-heading">
             Contact us
           </h4>
           <ul className="space-y-2">
             <li>
-              <a href="tel:+610392679030" className="hover:underline">
+              <a href="tel:+610392679030" className="hover:underline font-heading">
                +61 0392679030
               </a>
             </li>
             <li>
-              <a href="mailto:Enquiries@treasuredcareforyou.com" className="hover:underline">
+              <a href="mailto:Enquiries@treasuredcareforyou.com" className="hover:underline font-heading">
                 Enquiries@treasuredcareforyou.com
               </a>
             </li>
@@ -78,7 +78,7 @@ export default function Footer() {
 
         {/* Column 4 */}
         <div>
-          <h4 className="font-extrabold mb-3 text-2xl text-white">
+          <h4 className="font-extrabold mb-3 text-2xl text-white font-heading">
             Follow us on
           </h4>
           <ul className="flex space-x-4">
@@ -110,7 +110,7 @@ export default function Footer() {
       {/* Copyright & Links */}
       <div className="max-w-6xl mx-auto px-4 py-6 flex flex-col sm:flex-row justify-center items-center text-sm text-white space-y-4 sm:space-y-0 ">
         <span className="text-center font-extrabold text-1xl">
-          © 2025 Treasured care for you
+          © 2025 Treasured Care For You
         </span>
       </div>
     </footer>
