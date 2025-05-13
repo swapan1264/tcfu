ac8ebbaeb2ae228bc0011fe102418238495c05d6
swapan1264 <enquiries@treasuredcareforyou.com>
2025-05-13 08:06:05 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,21 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import "./globals.css";
+import { Montserrat, Lora } from 'next/font/google'
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400','600','700'],
+  variable: '--font-montserrat',
+  display: 'swap',
+})
+
+const lora = Lora({
+  subsets: ['latin'],
+  weight: ['400','700'],
+  variable: '--font-lora',
+  display: 'swap',
+})
 export const metadata = {
   title: 'Treasured Care For You',
   description: 'Providing nurturing support and care services',
@@ -9,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en"  className={`${montserrat.variable} ${lora.variable}`}>
           <head>
         <link rel="icon" href="/logo.jpg" sizes="128x128" type="image/png" />
       </head>
